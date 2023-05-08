5e79c017342bce993c5b259f0e4758fd1a7f89c3
sanqiang Jiang <54235991+sanqiangj@users.noreply.github.com>
2023-05-08 21:34:46 +1000

    Create clean.py
    just transfer

---
+++ twitter_process/clean.py
@@ -0,0 +1,10 @@
+def select_housing(input_json):
+    housing_words = ["rent","renting","rental","neighborhood","room","real estate", "house","housing","eviction",
+                     "construction","shelter","apartment", "lease", "tenant", "landlord","building", 
+                     "deposit", "utilities", "sublet", "roommate", "vacancy", "property"]
+    
+
+def select_income(input_json):
+    income_words = ["income", "salary", "wage", "earnings","money","paycheck", "benefits", "bonus", "commission", 
+                    "taxes", "deductions", "payroll", "financial", "investment", "profit", "revenue","earn","budget"
+                    "savings",]
